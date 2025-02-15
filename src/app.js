f8b13a8e445ccc51e1369971b2e70e99cdcd2709
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2025-02-16 01:15:25 +0530

    exploring http methods, app.use

--- src/app.js
+++ src/app.js
@@ -2,13 +2,34 @@ const express = require('express');
 
 const app = express(); // create express app
 
-app.get("/", (req, res) => {
-    res.send('Hello from server!');
-}); // callback function is request handler
+// order of routes matter
+// request handlers
 
-app.get("/test", (req, res) => {
-    res.send('Hello from test server!');
+app.use("/user", (req, res) => {
+    res.send('app.use');
+});
+
+
+app.get("/user", (req, res) => {
+    res.send('Get user data');
+});
+
+app.post("/user", (req, res) => {
+    res.send('Send user data');
+});
+
+app.delete("/user", (req, res) => {
+    res.send('Delete user data');
+});
+
+app.patch("/user", (req, res) => {
+    res.send('Patch user data');
+});
+
+app.put("/user", (req, res) => {
+    res.send('Put user data');
 });
 
 app.listen(3000, () => {console.log("Listening on PORT 3000")}); // listen for incoming requests
+
 
