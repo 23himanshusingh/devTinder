67d5d147b1e01c7a51017282317f69a2f3bc4ba8
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2025-02-16 11:49:30 +0530

    Multiple route handlers and next , res.send

--- src/app.js
+++ src/app.js
@@ -5,30 +5,46 @@ const app = express(); // create express app
 // order of routes matter
 // request handlers
 
-app.use("/user", (req, res) => {
-    res.send('app.use');
-});
+
+app.get("/user", 
+[(req, res, next) => {
+    next();
+    console.log('Route Handler 1');
+    // res.send('Response from route handler 1');
+},
+(req, res, next) => {
+    // res.send('Response from route handler 2');
+    console.log('Route Handler 2');
+    next();
+}],
+[(req, res, next) => {
+    console.log('Route Handler 3');
+    next();
+    // res.send('Response from route handler 3');
+}]
+
+);
 
 
-app.get("/user", (req, res) => {
-    res.send('Get user data');
-});
+// app.get("/user", (req, res) => {
+//     res.send('Get user data');
+// });
 
-app.post("/user", (req, res) => {
-    res.send('Send user data');
-});
+// app.post("/user", (req, res) => {
+//     res.send('Send user data');
+// });
 
-app.delete("/user", (req, res) => {
-    res.send('Delete user data');
-});
+// app.delete("/user", (req, res) => {
+//     res.send('Delete user data');
+// });
 
-app.patch("/user", (req, res) => {
-    res.send('Patch user data');
-});
+// app.patch("/user", (req, res) => {
+//     res.send('Patch user data');
+// });
 
-app.put("/user", (req, res) => {
-    res.send('Put user data');
-});
+// app.put("/user", (req, res) => {
+//     res.send('Put user data');
+// });
 
 app.listen(3000, () => {console.log("Listening on PORT 3000")}); // listen for incoming requests
 
