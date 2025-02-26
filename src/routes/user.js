c97428ac9921af1e62067a5b6ae9c5fa4dde5cd1
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2025-02-26 11:27:33 +0530

    Increased default limit for user feed pagination from 5 to 20, with a maximum limit of 50.

--- src/routes/user.js
+++ src/routes/user.js
@@ -48,7 +48,7 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
     try{
         const loggedInUser = req.user;
         const page = parseInt(req.query.page) || 1;
-        let limit = parseInt(req.query.limit) || 5;
+        let limit = parseInt(req.query.limit) || 20;
         limit = limit > 50 ? 50 : limit;
         const skip = (page-1)*limit;
         // get users which have (send or received) request from the loggedInUser
