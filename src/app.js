103dfeedd2449e03cd241246ab2b0895fbae4705
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2025-02-27 13:04:20 +0530

    Added chat route and integrated with chat model for user messaging

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express(); // create express app
 const connectDB = require("./config/database"); // import database connection
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const http = require("http");
+
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
-const cors = require("cors");
-const http = require("http");
 const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use(
   cors({
@@ -19,9 +21,6 @@ app.use(
   })
 );
 
-
-
-
 app.use(express.json()); // parse JSON bodies
 app.use(cookieParser()); // parse cookies in the request headers to make it accessible
 
@@ -29,6 +28,7 @@ app.use("/", authRouter); // add authRouter to the app
 app.use("/", profileRouter); // add profileRouter to the app
 app.use("/", requestRouter); // add requestRouter to the app
 app.use("/", userRouter); // add userRouter to the app
+app.use("/", chatRouter); // add chatRouter to the app
 
 const server = http.createServer(app);
 initializeSocket(server);
