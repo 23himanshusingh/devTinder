85d722ed983a309c93e7111e09dea7ec3c7668ee
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2025-02-27 12:28:11 +0530

    Added chat model and implement message handling in Socket.IO for joinChat and sendMessage events after  connection

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,22 +1,64 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+const { Chat } = require("../models/chat");
+const ConnectionRequest = require("../models/connectionRequest");
+
+const getSecretRoomId = (userId, taargetId) => {
+    return crypto
+        .createHash("sha256")
+        .update([userId, targetUserId].sort().join('$'))
+        .digest("hex");
+};
 
 const initializeSocket = (server) => {
-    const io = socket(server,{
-        cors:{
+    const io = socket(server, {
+        cors: {
             origin: "http://localhost:5173",
         }
     });
-    io.on("connection",(socket)=>{
+    io.on("connection", (socket) => {
         //handle events
-        socket.on("joinChat",({firstName,userId,
-            targetUserId
-        }) => {
-            const roomId = [userId,targetUserId].sort().join("_");
-            console.log(firstName+" joined Room : " + roomId);
+        socket.on("joinChat", ({firstName,userId,targetUserId}) => {
+            const roomId = getSecretRoomId(userId, targetUserId);
+            console.log(firstName + " joined Room : " + roomId);
             socket.join(roomId);
         });
-        socket.on("sendMessage",()=>{});
-        socket.on("disconnect",()=>{});
+        socket.on("sendMessage", async ({firstName, lastName,userId, targetUserId,text}) => {
+            //Save messages to the database
+            try{
+                const roomId = getSecretRoomId(userId, targetUserId);
+                const isConnection = await ConnectionRequest.findOne({
+                    $or : [
+                        {fromUserId :userId, toUserId :targetUserId, status: "accepted"},
+                        {fromUserId :targetUserId, toUserId :userId, status: "accepted"},
+                    ]
+                });
+                if (!isConnection) return res.status(401).send(
+                    "Requested target user id not in connections"
+                );
+                let chat = await Chat.findOne({
+                    participants: {$all : [userId, targetUserId]},
+                });
+                if (!chat){
+                    chat = new Chat({
+                        participants: [userId, targetUserId],
+                        messages: [],
+                    });
+                }
+
+                chat.messages.push({
+                    senderId: userId,
+                    text,
+                });
+                await chat.save();
+                io.to(roomId).emit("messageReceived",{
+                    firstName,lastName,text
+                });
+            }catch(err){
+                resizeBy.send(err);
+            }
+        });
+        socket.on("disconnect", () => {});
     });
 };
 
